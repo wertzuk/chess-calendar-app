c9c84a1b4dd6ef19b11539dcabd6ebdb4de11a49
wertzuk <pascalb95@gmx.de>
2024-10-16 18:32:56 +0200

    add tournament factory and seeder

--- database/factories/TournamentFactory.php
+++ database/factories/TournamentFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use \App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tournament>
@@ -16,8 +17,15 @@ class TournamentFactory extends Factory
      */
     public function definition(): array
     {
+
+        $date = fake()->dateTimeBetween('+0 days', '+2 years');
         return [
-            //
+            'title' => fake()->text(50),
+            'user_id' => User::factory(),
+            'chess_type' => fake()->randomElement(['Blitz', 'Schnellschach', 'Klassisch']),
+            'time_control' => fake()->numberBetween(1,100) . '+0',
+            'start_date' => $date,
+            'end_date' => $date->add(new \DateInterval('P7D')),
         ];
     }
 }
