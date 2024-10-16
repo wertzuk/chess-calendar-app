6a853d43ee7b2d8f8b1b85baa7584b682bf9de5d
wertzuk <pascalb95@gmx.de>
2024-10-16 15:37:10 +0200

    use the created components in home screen

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Card from '@/Components/Card.vue';
 import MainLayout from '@/Layouts/MainLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
@@ -13,8 +14,12 @@ import { Head } from '@inertiajs/vue3';
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        You're logged in!
+                        <p v-if="$page.props.auth.user">You're logged in!</p>
+                        <p v-else>Hello guest!</p>
                     </div>
+                </div>
+                <div class="mt-8">
+                    <Card />
                 </div>
             </div>
         </div>
--- resources/js/app.js
+++ resources/js/app.js
@@ -13,7 +13,7 @@ createInertiaApp({
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
+            import.meta.glob('./Pages/**/*.vue')
         ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
