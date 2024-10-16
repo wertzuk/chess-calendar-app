45f394cb8618a83e3e3d9357d7d5d98544d25906
wertzuk <pascalb95@gmx.de>
2024-10-16 18:08:55 +0200

    add migration for tournaments

--- database/migrations/2024_10_16_155340_create_tournaments_table.php
+++ database/migrations/2024_10_16_155340_create_tournaments_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -14,6 +15,26 @@ return new class extends Migration
         Schema::create('tournaments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignIdFor(User::class);
+            $table->string('title');
+            $table->string('time_control');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('game_type', ['Blitz', 'Schnellschach', 'Klassisch']);
+            $table->string('prize_fund')->nullable();
+            $table->string('format')->nullable();
+            $table->string('organizer')->nullable();
+            $table->string('street')->nullable();
+            $table->integer('plz')->nullable();
+            $table->integer('number_of_rounds')->nullable();
+            $table->boolean('elo_rated')->default(0);
+            $table->boolean('blitz_elo_rated')->default(0);
+            $table->boolean('rapid_elo_rated')->default(0);
+            $table->boolean('dwz_rated')->default(0);
+            $table->string('chess_results_link')->nullable();
+            $table->string('website_link')->nullable();
+            $table->string('announcement_link')->nullable();
+            $table->text('description')->nullable();
         });
     }
 
