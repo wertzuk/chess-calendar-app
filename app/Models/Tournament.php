856bd1c2f0f50edc64794ee571b886b717ba384c
wertzuk <pascalb95@gmx.de>
2024-10-16 18:15:59 +0200

    define reltions between users and tournaments

--- app/Models/Tournament.php
+++ app/Models/Tournament.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tournament extends Model
 {
     /** @use HasFactory<\Database\Factories\TournamentFactory> */
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class)->withDefault();
+    }
 }
