eef1a652fa29a96c8c43b6de1c255239ddcd2de6
wertzuk <pascalb95@gmx.de>
2024-10-16 15:35:28 +0200

    create component for Chip and Card

---
+++ resources/js/Components/Chip.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <span
+            class="inline-flex items-center px-3 py-1 bg-gray-200 text-gray-700 text-sm font-medium rounded-full dark:bg-gray-700 dark:text-gray-300"
+        >
+            <slot></slot>
+        </span>
+    </div>
+</template>
