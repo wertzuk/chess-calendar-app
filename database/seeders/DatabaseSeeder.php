360be8e53d534b479d11f96ab9778173d3b44701
wertzuk <pascalb95@gmx.de>
2024-10-18 21:46:20 +0200

    create seeder for dummy user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Tournament;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Tournament::factory()->count(10)->create();
+        User::create([
+            'name' => 'Pascal',
+            'email' => 'pascal@test.com',
+            'password' => '$2y$12$NH9SbHbqlBYsgPhHGaikSOPlZJL5oYKWiPivMQK6CjFsWnYVVIecS',
+            'remember_token' => 'NQb1cvwjoAYEm9M6r0rGTMSIyRX7b7g2AHaQZb6w5zV3Qp1cbvZjt7mFpQm9',
+        ]);
     }
 }
