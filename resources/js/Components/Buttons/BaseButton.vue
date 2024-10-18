5f55f64eb57bdebf3480f7c3d99bb97b15e280bf
wertzuk <pascalb95@gmx.de>
2024-10-18 22:11:09 +0200

    create button components

---
+++ resources/js/Components/Buttons/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <button
+        :class="[
+            'font-medium rounded-lg text-sm px-5 py-2.5 mb-2 focus:outline-none',
+            customClasses,
+            ringClasses,
+            hoverClasses,
+            darkModeClasses,
+        ]"
+        :type="type"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<script setup>
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'button',
+    },
+    customClasses: {
+        type: String,
+        default: '', // Allow extending with specific classes
+    },
+    ringClasses: {
+        type: String,
+        default: '', // Focus ring styling
+    },
+    hoverClasses: {
+        type: String,
+        default: '', // Hover state styling
+    },
+    darkModeClasses: {
+        type: String,
+        default: '', // Dark mode-specific styling
+    },
+});
+</script>
---
+++ resources/js/Components/Buttons/IconButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <BaseButton
+        customClasses="text-white bg-blue-700 inline-flex items-center"
+        ringClasses="focus:ring-4 focus:ring-blue-300"
+        hoverClasses="hover:bg-blue-800"
+        darkModeClasses="dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+        <svg
+            class="w-4 h-4 me-2 text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 12h14m-7 7V5"
+            />
+        </svg>
+
+        <slot></slot>
+    </BaseButton>
+</template>
+
+<script setup>
+import BaseButton from './BaseButton.vue';
+</script>
