bf8b290f9d4ba9c295b8737586a24a89f3f42b41
wertzuk <pascalb95@gmx.de>
2024-10-19 16:25:22 +0200

    create component for rendering forms

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,35 +2,35 @@
     <Head title="Home" />
 
     <MainLayout>
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12">
-            <div class="mb-10">
-                <IconButton>Turnier erstellen</IconButton>
-            </div>
-            <div
-                class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
-            ></div>
-            <div v-for="(group, month) in groupedTournaments" :key="date">
-                <ol
-                    class="relative border-s border-gray-200 dark:border-gray-700"
-                >
-                    <li class="ms-4 pb-12">
-                        <div
-                            class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
-                        ></div>
-                        <time
-                            class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-                            >{{ month }}</time
-                        >
-                        <div class="mt-2 grid grid-cols-fill-300 gap-4 mx-auto">
-                            <TournamentCard
-                                :tournament="tournament"
-                                v-for="tournament in group"
-                                :key="tournament.id"
-                            />
-                        </div>
-                    </li>
-                </ol>
-            </div>
+        <div class="mb-10">
+            <IconButton
+                ><a :href="route('tournaments.create')"
+                    >Turnier erstellen</a
+                ></IconButton
+            >
+        </div>
+        <div
+            class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
+        ></div>
+        <div v-for="(group, month) in groupedTournaments" :key="date">
+            <ol class="relative border-s border-gray-200 dark:border-gray-700">
+                <li class="ms-4 pb-12">
+                    <div
+                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+                    ></div>
+                    <time
+                        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+                        >{{ month }}</time
+                    >
+                    <div class="mt-2 grid grid-cols-fill-300 gap-4 mx-auto">
+                        <TournamentCard
+                            :tournament="tournament"
+                            v-for="tournament in group"
+                            :key="tournament.id"
+                        />
+                    </div>
+                </li>
+            </ol>
         </div>
     </MainLayout>
 </template>
@@ -41,13 +41,11 @@ import TournamentCard from '@/Components/TournamentCard.vue';
 import MainLayout from '@/Layouts/MainLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import { usePage } from '@inertiajs/vue3';
-import PrimaryButton from '@/Components/Buttons/PrimaryButton.vue';
 import IconButton from '@/Components/Buttons/IconButton.vue';
-const tournaments = computed(() => usePage().props.tournaments);
 
+const tournaments = computed(() => usePage().props.tournaments);
 groupTournaments(tournaments.value);
 const groupedTournaments = computed(() => groupTournaments(tournaments.value));
-console.log(groupedTournaments.value);
 
 /**
  * Group tournaments by month
---
+++ resources/js/Pages/Tournaments/Create.vue
@@ -0,0 +1,15 @@
+<template>
+    <MainLayout>
+        <FormInput modelValue="test" type="email" v-model="test" />
+        {{ test }}
+    </MainLayout>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import FormInput from '@/Components/Form/FormInput.vue';
+import MainLayout from '@/Layouts/MainLayout.vue';
+const form = ref({});
+
+const test = ref(null);
+</script>
