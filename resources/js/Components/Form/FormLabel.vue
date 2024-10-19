f1c5e97a7d53bdbe98c34fc65c7be2ebd35e174c
wertzuk <pascalb95@gmx.de>
2024-10-19 21:08:05 +0200

    create separate component for input label

---
+++ resources/js/Components/Form/FormLabel.vue
@@ -0,0 +1,13 @@
+<template>
+    <label
+        :for="key"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        ><slot></slot
+    ></label>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    key: any;
+}>();
+</script>
