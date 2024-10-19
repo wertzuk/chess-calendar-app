229d287e310b3cc53bbeaf2d1555d28df7f07304
wertzuk <pascalb95@gmx.de>
2024-10-19 15:35:42 +0200

    enable crud operation routes on tournaments

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TournamentController::class, 'index'])->name('home');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('tournaments', TournamentController::class)->except('index');
 });
 
 require __DIR__.'/auth.php';
