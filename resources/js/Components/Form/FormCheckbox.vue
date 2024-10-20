e0c7e84aee6fc693fa9b4d22127a18422d97ed89
wertzuk <pascalb95@gmx.de>
2024-10-20 11:47:08 +0200

    create form checkbox component

---
+++ resources/js/Components/Form/FormCheckbox.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex items-center mb-4">
+        <input
+            :id="fieldKey"
+            type="checkbox"
+            :name="fieldKey"
+            :checked="modelValue"
+            :value="currentValue"
+            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            @change="currentValue ^= 1"
+        />
+        <label
+            :for="fieldKey"
+            class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+            <slot />
+        </label>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const props = defineProps({
+    fieldKey: String,
+    modelValue: Number,
+});
+const currentValue = ref(props.modelValue);
+
+defineEmits(['update:modelValue']);
+</script>
+
+<style lang="scss" scoped></style>
