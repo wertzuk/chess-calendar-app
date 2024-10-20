2a720f6de1a1a4f899df1b6656ff62fe88d845bf
wertzuk <pascalb95@gmx.de>
2024-10-20 12:06:17 +0200

    add some more inputs fields to form

--- resources/js/Pages/Tournaments/Create.vue
+++ resources/js/Pages/Tournaments/Create.vue
@@ -1,21 +1,44 @@
 <template>
     <MainLayout>
         <!-- <h1>Turnier erstellen</h1> -->
-        <form
-            :action="route('tournaments.store')"
-            method="post"
-            class="flex flex-col gap-4"
-        >
+        <form :action="route('tournaments.store')" method="post">
             <input type="hidden" name="_token" :value="page.props.csrf_token" />
 
             <FormInput fieldKey="title">Turnier</FormInput>
             <FormDatePicker fieldKey="start_date">Startdatum</FormDatePicker>
             <FormDatePicker fieldKey="end_date">Enddatum</FormDatePicker>
+            <FormSelect
+                fieldKey="chess_type"
+                :options="['Klassisch', 'Schnellschach', 'Blitz']"
+                >Form</FormSelect
+            >
             <FormInput fieldKey="time_control">Zeitkontrolle</FormInput>
             <FormInput fieldKey="street">Straße</FormInput>
             <FormInput fieldKey="plz">PLZ</FormInput>
             <FormInput fieldKey="city">Ort</FormInput>
+            <FormInput fieldKey="organizer">Ausrichter</FormInput>
+
             <FormInput fieldKey="time_control">Zeitkontrolle</FormInput>
+            <FormCheckbox fieldKey="elo_rated">ELO Auswertung</FormCheckbox>
+            <FormCheckbox fieldKey="dwz_rated">DWZ Auswertung</FormCheckbox>
+            <FormCheckbox fieldKey="rapid_elo_rated"
+                >Rapid ELO Auswertung</FormCheckbox
+            >
+            <FormCheckbox fieldKey="blitz_elo_rated"
+                >Blitz ELO Auswertung</FormCheckbox
+            >
+            <FormInput type="url" fieldKey="chess_results_link"
+                >Chess-Results Link</FormInput
+            >
+            <FormInput type="url" fieldKey="website_link"
+                >Link zur Webseite</FormInput
+            >
+            <FormInput type="url" fieldKey="announcement_link"
+                >Link zur Ausschreibung</FormInput
+            >
+            <FormInput fieldKey="number_of_rounds" type="number"
+                >Anzahl Runden</FormInput
+            >
 
             <PrimaryButton class="mt-8">Erstellen</PrimaryButton>
         </form>
@@ -29,6 +52,8 @@ import MainLayout from '@/Layouts/MainLayout.vue';
 import PrimaryButton from '@/Components/Buttons/PrimaryButton.vue';
 import FormDatePicker from '@/Components/Form/FormDatePicker.vue';
 import { usePage } from '@inertiajs/vue3';
+import FormSelect from '@/Components/Form/FormSelect.vue';
+import FormCheckbox from '@/Components/Form/FormCheckbox.vue';
 const form = ref({});
 
 const page = usePage();
