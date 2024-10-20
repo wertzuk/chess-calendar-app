092a5cb02ab73d919ee5c5d2597236ffd3e75a4c
wertzuk <pascalb95@gmx.de>
2024-10-20 12:07:16 +0200

    don't expose tournament timestamps to frontend

--- app/Models/Tournament.php
+++ app/Models/Tournament.php
@@ -11,6 +11,10 @@ class Tournament extends Model
     /** @use HasFactory<\Database\Factories\TournamentFactory> */
     use HasFactory;
 
+    protected $guarded = ['id'];
+
+    protected $hidden = ['created_at', 'updated_at'];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
