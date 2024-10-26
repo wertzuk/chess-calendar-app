e591546f0ac7c70bb729798f38059c4a60c40af4
wertzuk <pascalb95@gmx.de>
2024-10-26 18:18:51 +0200

    create view for tournament edit

---
+++ resources/js/Pages/Tournaments/Edit.vue
@@ -0,0 +1,21 @@
+<template>
+    <MainLayout>
+        <SecondaryHeading class="text-center mb-6">Turnier bearbeiten</SecondaryHeading>
+        <FormTournament :tournament="tournament" @submitForm="submit"></FormTournament>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+import SecondaryHeading from '@/Components/SecondaryHeading.vue';
+import FormTournament from '@/Components/Form/FormTournament.vue';
+import { computed } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+const tournament = computed(() => usePage().props.tournament);
+
+function submit(form) {
+    form.put(`/tournaments/${tournament.value.id}`);
+}
+
+let props = defineProps(['errors']);
+</script>
