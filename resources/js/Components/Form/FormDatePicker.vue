dc1bff19ff559342e7869846e73bd69bd8c09256
wertzuk <pascalb95@gmx.de>
2024-11-01 11:11:05 +0100

    use calendar svg in date picker

--- resources/js/Components/Form/FormDatePicker.vue
+++ resources/js/Components/Form/FormDatePicker.vue
@@ -3,17 +3,7 @@
         <FormLabel :for="fieldKey"><slot></slot></FormLabel>
         <div class="relative max-w-sm mb-6">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                <svg
-                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                >
-                    <path
-                        d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
-                    />
-                </svg>
+                <IconCalendar />
             </div>
             <input
                 :value="modelValue"
@@ -36,6 +26,7 @@
 import { onBeforeUnmount, onMounted, ref } from 'vue';
 import { Datepicker } from 'flowbite';
 import FormLabel from './FormLabel.vue';
+import IconCalendar from '../Icons/IconCalendar.vue';
 
 defineOptions({
     inheritAttrs: false,
@@ -45,11 +36,15 @@ const datePicker = ref(null);
 
 onMounted(() => {
     const dateInput = document.getElementById(props.fieldKey);
+
+    const options = {
+        autohide: true,
+        format: 'dd.mm.yyyy',
+        rangePicker: false,
+    };
+
     if (dateInput) {
-        datePicker.value = new Datepicker(dateInput, {
-            autohide: true,
-            format: 'yyyy-mm-dd',
-        });
+        datePicker.value = new Datepicker(dateInput, options);
     }
 });
 
