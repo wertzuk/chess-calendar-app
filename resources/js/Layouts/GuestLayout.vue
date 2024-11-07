69fa46c67c64b20c49677d360b44c010a8b7da36
wertzuk <pascalb95@gmx.de>
2024-11-07 20:59:20 +0100

    remove logo

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import SecondaryHeading from '@/Components/Common/SecondaryHeading.vue';
 import { Link } from '@inertiajs/vue3';
 </script>
 
@@ -7,11 +8,9 @@ import { Link } from '@inertiajs/vue3';
     <div
         class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900"
     >
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
-            </Link>
-        </div>
+        <SecondaryHeading>
+            <Link href="/"> schachturniere.live </Link>
+        </SecondaryHeading>
 
         <div
             class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800"
