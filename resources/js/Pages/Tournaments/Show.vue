398ac45062c76a55857501ce921cfbc5c4dfa576
wertzuk <pascalb95@gmx.de>
2024-11-07 20:59:03 +0100

    create component to view single tournament

---
+++ resources/js/Pages/Tournaments/Show.vue
@@ -0,0 +1,3 @@
+<template>Show is working!</template>
+
+<script setup></script>
--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,15 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TournamentController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', [TournamentController::class, 'index'])->name('home');
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::resource('tournaments', TournamentController::class)->except('index');
+    Route::resource('tournaments', TournamentController::class)->except(['index', 'show']);
 });
+
+Route::get('/', [TournamentController::class, 'index'])->name('home');
+Route::get('/tournaments/{tournament}', [TournamentController::class, 'show'])->name('tournament.show');
 
 require __DIR__.'/auth.php';
 
