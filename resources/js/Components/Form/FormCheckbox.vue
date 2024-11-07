4f21809f1a1e3bc6e86cf1960c9d6f5fbfb48955
wertzuk <pascalb95@gmx.de>
2024-11-07 23:01:45 +0100

    fix bug in checkbox v-model

--- resources/js/Components/Form/FormCheckbox.vue
+++ resources/js/Components/Form/FormCheckbox.vue
@@ -5,9 +5,9 @@
             :id="fieldKey"
             :name="fieldKey"
             :checked="modelValue"
-            :value="currentValue"
+            :value="modelValue"
             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            @change="currentValue ^= 1"
+            @change="$emit('update:modelValue', ($event.target.value ^= 1))"
         />
         <label :for="fieldKey" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
             <slot />
@@ -16,12 +16,10 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
 const props = defineProps({
     fieldKey: String,
     modelValue: Number,
 });
-const currentValue = ref(props.modelValue);
 
 defineEmits(['update:modelValue']);
 </script>
