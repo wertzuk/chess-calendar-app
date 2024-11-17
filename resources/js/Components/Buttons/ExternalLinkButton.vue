abd6b429e56abd1424d62400f840416ebaa941a3
wertzuk <pascalb95@gmx.de>
2024-11-17 19:24:06 +0100

    create button for external links

---
+++ resources/js/Components/Buttons/ExternalLinkButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <a
+        href="#"
+        target="_blank"
+        class="inline-flex items-center gap-2.5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        ><slot></slot>
+        <svg
+            class="w-6 h-6 text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M19 12H5m14 0-4 4m4-4-4-4"
+            />
+        </svg>
+    </a>
+</template>
