fabef1a14412fa6059b24c9c6006d5fed186806e
wertzuk <pascalb95@gmx.de>
2024-11-17 15:54:31 +0100

    fix some toast stylings

--- resources/js/Components/Toast/ToastSuccess.vue
+++ resources/js/Components/Toast/ToastSuccess.vue
@@ -1,7 +1,7 @@
 <template>
     <div
         id="toast-success"
-        class="fixed right-10 bottom-0 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+        class="fixed inset-x-0 mx-auto sm:inset-x-auto sm:right-10 sm:mx-0 bottom-0 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-950"
         role="alert"
     >
         <div
@@ -23,7 +23,7 @@
         <div class="ms-3 text-sm font-normal"><slot></slot></div>
         <button
             type="button"
-            class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-950 dark:hover:bg-gray-700"
             data-dismiss-target="#toast-success"
             aria-label="Close"
         >
