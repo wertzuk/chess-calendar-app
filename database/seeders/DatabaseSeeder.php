dfa9faa6fccf1341f416bc6c724b4d7a47605a56
wertzuk <pascalb95@gmx.de>
2024-11-20 20:19:34 +0100

    clear seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Tournament::factory()->count(10)->create();
-        User::create([
-            'name' => 'Pascal',
-            'email' => 'pascal@test.com',
-            'password' => '$2y$12$NH9SbHbqlBYsgPhHGaikSOPlZJL5oYKWiPivMQK6CjFsWnYVVIecS',
-            'remember_token' => 'NQb1cvwjoAYEm9M6r0rGTMSIyRX7b7g2AHaQZb6w5zV3Qp1cbvZjt7mFpQm9',
-        ]);
+        // Tournament::factory()->count(10)->create();
     }
 }
