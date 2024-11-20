8b7ba9b88fc886ca13defbb1fe12d14661921f0f
wertzuk <pascalb95@gmx.de>
2024-11-20 22:05:42 +0100

    add icon and floating button

---
+++ resources/js/Components/Buttons/FloatingButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import IconAdd from '../Icons/IconAdd.vue';
+</script>
+
+<template>
+    <Link
+        href="#"
+        class="fixed bottom-5 z-10 sm:bottom-20 right-4 sm:right-[10%] flex gap-2 items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    >
+        <IconAdd />
+        <span>Turnier erstellen</span>
+    </Link>
+</template>
---
+++ resources/js/Components/Icons/IconAdd.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg
+        class="w-[20px] h-[20px] text-gray-800 dark:text-white"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+    >
+        <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M5 12h14m-7 7V5"
+        />
+    </svg>
+</template>
