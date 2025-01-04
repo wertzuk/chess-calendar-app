61daf845bd9d6c1a54ebe13d2dd82a3540c07046
wertzuk <pascalb95@gmx.de>
2025-01-04 15:45:18 +0100

    darken external link button

--- resources/js/Components/Buttons/ExternalLinkButton.vue
+++ resources/js/Components/Buttons/ExternalLinkButton.vue
@@ -2,7 +2,7 @@
     <a
         href="#"
         target="_blank"
-        class="inline-flex items-center gap-2.5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        class="inline-flex py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-900 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
         ><slot></slot>
         <svg
             class="w-6 h-6 text-gray-800 dark:text-white"
