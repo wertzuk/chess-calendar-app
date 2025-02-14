d9844142913ade4f82d0c651a815e25b575df424
wertzuk <pascalb95@gmx.de>
2025-02-14 17:00:19 +0100

    lighted up text

--- resources/js/Components/DescriptionList/DescriptionListItem.vue
+++ resources/js/Components/DescriptionList/DescriptionListItem.vue
@@ -3,7 +3,7 @@
         <dt class="text-sm/6 font-medium text-gray-900 dark:text-white">
             <slot name="label">Default Label</slot>
         </dt>
-        <dd class="mt-1 text-sm/6 text-gray-700 dark:text-gray-500 sm:col-span-2 sm:mt-0">
+        <dd class="mt-1 text-sm/6 text-gray-500 dark:text-gray-400 sm:col-span-2 sm:mt-0">
             <slot name="value">Default Value</slot>
         </dd>
     </div>
