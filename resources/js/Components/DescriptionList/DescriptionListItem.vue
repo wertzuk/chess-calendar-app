0cebb1831824aff74ef1919c9095d49e71b7cab7
wertzuk <pascalb95@gmx.de>
2025-02-14 11:20:46 +0100

    use description list item

---
+++ resources/js/Components/DescriptionList/DescriptionListItem.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt class="text-sm/6 font-medium text-gray-900 dark:text-white">
+            <slot name="label">Default Label</slot>
+        </dt>
+        <dd class="mt-1 text-sm/6 text-gray-700 dark:text-gray-500 sm:col-span-2 sm:mt-0">
+            <slot name="value">Default Value</slot>
+        </dd>
+    </div>
+</template>
--- resources/js/Components/List/ListItem.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <li class="flex items-center space-x-3 rtl:space-x-reverse">
-        <slot></slot>
-    </li>
-</template>
