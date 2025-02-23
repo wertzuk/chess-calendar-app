5d4ecd8f862cf871df9982980781bc277e46edd6
wertzuk <pascalb95@gmx.de>
2025-02-23 16:30:03 +0100

    use dropdown menu for crud operations

--- resources/js/Components/TournamentCard.vue
+++ resources/js/Components/TournamentCard.vue
@@ -1,10 +1,50 @@
 <template>
     <div
-        class="p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col"
+        class="p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 duration-200 flex flex-col cursor-pointer"
     >
-        <h5 class="mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {{ tournament.title }}
-        </h5>
+        <div class="flex justify-between gap-2 relative">
+            <h5 class="mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {{ tournament.title }}
+            </h5>
+            <div class="flex flex-none" v-if="tournament.can.delete || tournament.can.edit">
+                <button
+                    :id="`dropdown_${tournament.id}`"
+                    :data-dropdown-toggle="`dropdownDots_${tournament.id}`"
+                    class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    type="button"
+                >
+                    <IconDots />
+                </button>
+                <div
+                    :id="`dropdownDots_${tournament.id}`"
+                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600"
+                >
+                    <ul
+                        class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                        :aria-labelledby="`dropdown_${tournament.id}`"
+                    >
+                        <li v-if="tournament.can.edit">
+                            <Link
+                                :href="route('tournaments.edit', { id: tournament.id })"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                >Bearbeiten</Link
+                            >
+                        </li>
+                        <li v-if="tournament.can.delete">
+                            <div
+                                href="#"
+                                @click="destroy"
+                                data-modal-target="popup-modal"
+                                data-modal-toggle="popup-modal"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            >
+                                Löschen
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <DateRange
             :start="tournament.start_date"
             :end="tournament.end_date"
@@ -21,42 +61,17 @@
             <Chip v-if="tournament.rapid_elo_rated">Rapid-ELO</Chip>
             <Chip v-if="tournament.blitz_elo_rated">Blitz-ELO</Chip>
         </div>
-        <div class="flex justify-between mt-8">
-            <LinkButton :href="route('tournament.show', { id: tournament.id })">Mehr</LinkButton>
-
-            <div class="flex gap-2">
-                <EditButton
-                    :href="route('tournaments.edit', { id: tournament.id })"
-                    v-if="tournament.can.edit"
-                    >Edit</EditButton
-                >
-                <form method="post" @submit.prevent="destroy">
-                    <DeleteButton
-                        v-if="tournament.can.delete"
-                        @click="destroy"
-                        data-modal-target="popup-modal"
-                        data-modal-toggle="popup-modal"
-                        ><IconDelete></IconDelete
-                    ></DeleteButton>
-                </form>
-            </div>
-        </div>
     </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
+import { initFlowbite } from 'flowbite';
+import { Link, useForm } from '@inertiajs/vue3';
 import Chip from '@/Components/Common/Chip.vue';
 import IconLocation from '@/Components/Icons/IconLocation.vue';
-import LinkButton from '@/Components/Buttons/LinkButton.vue';
-import IconEdit from '@/Components/Icons/IconEdit.vue';
-import IconDelete from '@/Components/Icons/IconDelete.vue';
-import PrimaryButton from './Buttons/PrimaryButton.vue';
-import { initFlowbite } from 'flowbite';
-import { useForm } from '@inertiajs/vue3';
+import IconDots from '@/Components/Icons/IconDots.vue';
 import DateRange from './Common/DateRange.vue';
-import EditButton from './Buttons/EditButton.vue';
-import DeleteButton from './Buttons/DeleteButton.vue';
 
 const props = defineProps(['tournament']);
 const form = useForm({});
