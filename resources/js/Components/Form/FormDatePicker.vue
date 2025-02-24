8657db03553644bd04721f4ef0cf7eb6c1633569
wertzuk <pascalb95@gmx.de>
2025-02-24 22:42:04 +0100

    increase input font size to 16px
    This in necessary to prevent the auto zoom in when using Safari on a mobile device

--- resources/js/Components/Form/FormDatePicker.vue
+++ resources/js/Components/Form/FormDatePicker.vue
@@ -10,7 +10,7 @@
                 :id="fieldKey"
                 @changeDate="$emit('update:modelValue', $event.target.value)"
                 type="text"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Select date"
             />
         </div>
--- resources/js/Components/Form/FormSelect.vue
+++ resources/js/Components/Form/FormSelect.vue
@@ -8,7 +8,7 @@
             :name="fieldKey"
             @input="$emit('update:modelValue', $event.target.value)"
             :value="props.modelValue"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
             <option
                 v-for="option in options"
