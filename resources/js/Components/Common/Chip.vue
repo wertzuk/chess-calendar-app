a84b36af85b9d6591587c8473e5e808bd1da9eeb
wertzuk <pascalb95@gmx.de>
2025-04-13 13:29:37 +0200

    make badge text smaller

--- resources/js/Components/Common/Chip.vue
+++ resources/js/Components/Common/Chip.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <span
-            class="inline-flex uppercase items-center font-light px-3 py-1 bg-gray-200 text-gray-700 text-sm rounded-full dark:bg-gray-700 dark:text-gray-300"
+            class="inline-flex uppercase items-center font-light px-3 py-1 bg-gray-200 text-gray-700 text-xs rounded-full dark:bg-gray-700 dark:text-gray-300"
             :class="{
                 'dark:bg-emerald-900/50 dark:text-emerald-200': !isIconChip,
             }"
