bdd880219e7b0f37a722618a52100c5a48130bca
wertzuk <pascalb95@gmx.de>
2025-05-31 20:33:19 +0200

    set Filter Button light theme colors

--- resources/js/Components/Buttons/FilterButton.vue
+++ resources/js/Components/Buttons/FilterButton.vue
@@ -2,8 +2,12 @@
     <button
         class="py-3 px-4 text-sm rounded-lg flex items-center leading-none"
         :class="{
-            'dark:bg-gray-400 dark:text-gray-900': active,
-            'dark:bg-gray-950 dark:text-white': !active,
+            // ACTIVE STATE
+            'bg-neutral-900 text-neutral-100 dark:bg-neutral-200 dark:text-black': active,
+
+            // INACTIVE STATE
+            'bg-neutral-300 text-neutral-800 dark:bg-neutral-950 dark:text-neutral-200 hover:bg-neutral-200 dark:hover:bg-gray-800':
+                !active,
         }"
     >
         <slot />
