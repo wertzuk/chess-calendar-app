21e70cc0c16393b92f8576142948953c4400136d
wertzuk <pascalb95@gmx.de>
2025-05-31 20:11:12 +0200

    create custom Filter Button componsent

---
+++ resources/js/Components/Buttons/FilterButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <button
+        class="py-3 px-4 text-sm rounded-lg flex items-center leading-none"
+        :class="{
+            'dark:bg-gray-400 dark:text-gray-900': active,
+            'dark:bg-gray-950 dark:text-white': !active,
+        }"
+    >
+        <slot />
+    </button>
+</template>
+
+<script setup>
+defineProps({ active: Boolean });
+</script>
