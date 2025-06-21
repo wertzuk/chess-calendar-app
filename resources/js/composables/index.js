50aaab1e0df58c2af1748b319f7146e40caf18d1
Pascal Barzen <pascalb95@gmx.de>
2025-06-21 20:12:44 +0200

    export new composable in barrel file

--- resources/js/composables/index.js
+++ resources/js/composables/index.js
@@ -2,3 +2,4 @@ export { useTournaments } from './useTournaments';
 export { useInfiniteScroll } from './useInfiniteScroll';
 export { useSearch } from './useSearch';
 export { useFlashMessages } from './useFlashMessages';
+export { useNominatim } from './useNominatim';
