13e1d9f0d5d26df1e3a56800b6767f274ac6d787
Pascal Barzen <pascalb95@gmx.de>
2025-07-19 19:52:06 +0200

    remove unused import

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,5 +1,4 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import SecondaryHeading from '@/Components/Common/SecondaryHeading.vue';
 import { Link } from '@inertiajs/vue3';
 </script>
@@ -9,7 +8,7 @@ import { Link } from '@inertiajs/vue3';
         class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900"
     >
         <SecondaryHeading>
-            <Link href="/"> schachturniere.live </Link>
+            <Link href="/"> schachturniere.live</Link>
         </SecondaryHeading>
 
         <div
